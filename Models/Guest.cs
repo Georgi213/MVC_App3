448a218e949c0a7517939d9ebfd1f426947836ff
Opilane TTHK <opilane@tthk.ee>
2022-03-01 11:43:41 +0200

    Add project files.

---
+++ Models/Guest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_App.Models
+{
+    public class Guest
+    {
+        public int Id;
+        [Required(ErrorMessage = "On vaja sisesta oma nime!!!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "On vaja sisatada email!!!")]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "Viga emaili sisestamiseks!!!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Sisesta oma telefoni number!!!")]
+        [RegularExpression(@"\+372.+", ErrorMessage = "Vale telefoni number, Alguses on +372...")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Tee oma valik!!!")]
+        public bool? WillAttend { get; set; }
+    }
+}
